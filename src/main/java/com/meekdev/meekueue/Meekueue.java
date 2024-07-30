c9bc22001437e315772f0632e8ed0b7a5e674187
Meek <154077770+Meekiavelique@users.noreply.github.com>
2024-07-30 11:27:54 +0200

    Made the intiation message better

--- src/main/java/com/meekdev/meekueue/Meekueue.java
+++ src/main/java/com/meekdev/meekueue/Meekueue.java
@@ -19,6 +19,17 @@ public class Meekueue {
     private final ProxyServer server;
     private final Logger logger;
     private QueueManager queueManager;
+    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    private static final String ANSI_ORANGE = "\033[38;5;208m";
 
     @Inject
     public Meekueue(ProxyServer server, Logger logger) {
@@ -29,9 +40,29 @@ public class Meekueue {
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         queueManager = new QueueManager(this);
+        String fullVersion = server.getVersion().toString();
+        String versionNumber = extractVersionNumber(fullVersion);
+
+
         server.getEventManager().register(this, new QueueListener(queueManager));
         server.getCommandManager().register("queue", new QueueCommand(queueManager, this));
-        logger.info("Meekueue has been initialized.");
+        logger.info(ANSI_WHITE + "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁" + ANSI_RESET);
+        logger.info(" ");
+        logger.info("             [" + ANSI_ORANGE + "Meekueue " + ANSI_RESET + "Velocity]           ");
+        logger.info(String.format(ANSI_RESET + "The plugin is initiated running on version " + ANSI_ORANGE + Meekueue.VERSION + ANSI_RESET));
+        logger.info((ANSI_RESET + "Velocity Version : " + ANSI_ORANGE + versionNumber) + ANSI_RESET);
+        logger.info("Github : https://github.com/Meekiavelique/Meekueue");
+        logger.info(" ");
+        logger.info("▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁");
+    }
+    private String extractVersionNumber(String fullVersion) {
+        String[] parts = fullVersion.split(" ");
+        for (String part : parts) {
+            if (part.startsWith("3.")) {
+                return part;
+            }
+        }
+        return "Unknown";
     }
 
     public ProxyServer getServer() {
