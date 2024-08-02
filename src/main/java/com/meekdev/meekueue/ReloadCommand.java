7d93f6eb37b1f6323f7ea00a5300e6994cbd3381
ChairGamertag87 <76748554+ChairGamertag87@users.noreply.github.com>
2024-08-02 23:15:20 +0200

    There are nothing in just an inchident on the race
    it's usefull?

---
+++ src/main/java/com/meekdev/meekueue/ReloadCommand.java
@@ -0,0 +1,10 @@
+package com.meekdev.meekueue;
+
+import com.velocitypowered.api.command.SimpleCommand;
+import com.velocitypowered.api.proxy.Player;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import java.util.Optional;
+public class ReloadCommand {
+}
